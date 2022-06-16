cb9707ceb13a73a6c46fb059d8c29b5c356de830
LasseWardenaer <lasse.wardenar@gmail.com>
2022-06-16 20:58:25 +0200

    add submodule

--- source/main.cpp
+++ source/main.cpp
@@ -5,8 +5,40 @@
 // Initializes GLUT, the display mode, and main window; registers callbacks;
 // enters the main event loop.
 #include <iostream>
+#include <external/glfw/glfw3.h>
 
-int main(int argc, char** argv) {
-  std::cout << "Hello world! \n";
-  return 0;
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
